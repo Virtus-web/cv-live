8d0efd38f23fb57db3f216e2bb0790d66dd47fae
Virtus-web <nicolas.pedenau@hotmail.fr>
2022-03-18 16:37:35 +0100

    texte

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -4,7 +4,7 @@ function Profil() {
         <div className="profil mb5">
             <h2>Profil | Disponible immédiatement</h2>
             <p style={{textAlign: 'justify'}}>
-                Après une évolution positive dans le domaine des medias et de la communication, j'ai amorcé un changement de carrière orienté vers le développement informatique afin d'être plus cohérent avec mes objectifs professionnels et personnels. J'ai ainsi passé ces trois dernières années à me familiariser avec ce nouvel environnement dans un premier temps, puis à perfectionner mon travail afin d'acquérir des compétences techniques dans un cadre professionnel
+                Après une évolution positive dans le domaine des médias et de la communication, j'ai amorcé un changement de carrière orienté vers le développement informatique afin d'être plus cohérent avec mes objectifs professionnels et personnels. Durant ces trois dernières années, j'ai ainsi perfectionné mon travail afin d'acquérir les compétences techniques nécessaires à un cadre professionnel
             </p>
         </div>
     )
