58e22d28a8453aff35ff6333e88f062ed15d1949
virtus-web <nico@MSI.>
2024-05-14 14:25:57 +0200

    styling + sizing pdf

--- src/App.js
+++ src/App.js
@@ -19,18 +19,19 @@ function App() {
 
     const handleGenerateCv = () => {
         const input = pdfRef.current;
-        html2canvas(input).then((canvas) => {
+        html2canvas(input, {
+            scale: 1
+        }).then((canvas) => {
             const imgData = canvas.toDataURL('image/jpg');
-            const pdf = new jsPDF('p', 'mm', [297, 210], true);
+            const pdf = new jsPDF('p', 'mm', [210, 297]);
             const pdfWidth = pdf.internal.pageSize.getWidth();
             const pdfHeight = pdf.internal.pageSize.getHeight();
-            const imgWidth = canvas.width;
-            const imgHeight = canvas.height;
+            const imgWidth = pdfWidth;
+            const imgHeight = pdfHeight;
             const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
-            console.log(pdf.internal.pageSize.getHeight());
-            const imgX = (pdfWidth - imgWidth * ratio) / 2;
-            const imgY = 30;
-            pdf.addImage(imgData, 'jpg', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+            const imgX = 0;
+            const imgY = 0;
+            pdf.addImage(imgData, 'jpg', imgX, imgY * ratio, imgWidth * ratio, imgHeight * ratio);
             pdf.save(`CV_${name}-${lastName}`);
         })
     }
